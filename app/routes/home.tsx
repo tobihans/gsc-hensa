4bb02ef47bfc7dee459500a22d299d9365b29b8d
Hans B. K. Tognon <git@tobihans.space>
2025-04-28 10:27:40 +0100

    added a simple route for experimentation

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,7 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
@@ -9,5 +9,10 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div>
+      <h1>Nous sommes à HENSA</h1>
+      <Welcome />
+    </div>
+  );
 }
