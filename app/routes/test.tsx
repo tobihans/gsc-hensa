b62c1d8886922f0238f804519d42734c6d4ba76d
Hans B. K. Tognon <git@tobihans.space>
2025-04-28 11:29:45 +0100

    add bootstrap components for styling

--- app/routes/test.tsx
+++ app/routes/test.tsx
@@ -1,7 +1,10 @@
+import Button from "react-bootstrap/Button";
+
 export default function Test() {
   return (
     <div>
       <h1>C'est une page test</h1>
+      <Button>Click me !</Button>
     </div>
   );
 }
