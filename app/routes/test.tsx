d8372d6d0abe0a7f50c08c4aecc03a8842b0b685
Hans B. K. Tognon <git@tobihans.space>
2025-05-04 22:57:41 +0100

    chore: add a sample of how to use user instance

--- app/routes/test.tsx
+++ app/routes/test.tsx
@@ -1,9 +1,12 @@
 import Button from "react-bootstrap/Button";
+import { useAuth } from "~/contexts/auth";
 
 export default function Test() {
+  const { user } = useAuth();
+
   return (
     <div>
-      <h1>C'est une page test</h1>
+      <h1>{user?.email}</h1>
       <Button>Click me !</Button>
     </div>
   );
