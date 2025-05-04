549a8c4b611d6a9b104235946197943cbfb8e11f
Hans B. K. Tognon <git@tobihans.space>
2025-05-04 21:36:58 +0100

    feat: add a context provider to manage auth state

---
+++ app/contexts/auth.tsx
@@ -0,0 +1,44 @@
+import { onAuthStateChanged, type User } from "firebase/auth";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  type ReactNode,
+} from "react";
+import { auth } from "~/firebase.config";
+
+interface AuthContextType {
+  user: User | null;
+  setUser: (user: User | null) => void;
+}
+
+export const AuthContext = createContext<AuthContextType | null>(null);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user: User | null) => {
+      setUser(user);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error("Missing context. Wrap your component in AuthProvider.");
+  }
+
+  return context;
+};
