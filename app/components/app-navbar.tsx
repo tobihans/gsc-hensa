648bdf46df77eec45669f024b5f04928ef9652f4
Hans B. K. Tognon <git@tobihans.space>
2025-05-05 10:57:27 +0100

    feat: update navbar to reflect our project needs

--- app/components/app-navbar.tsx
+++ app/components/app-navbar.tsx
@@ -1,33 +1,42 @@
-import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Offcanvas from "react-bootstrap/Offcanvas";
+import { Link } from "react-router";
 
 export function AppNavbar() {
   return (
     <>
       <Navbar expand="sm" className="bg-body-tertiary mb-3">
         <Container fluid>
-          <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
+          <Navbar.Brand>
+            <Link to="/" className="navbar-brand">
+              GSC
+            </Link>
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="offcanvasNavbar" />
           <Navbar.Offcanvas
             id="offcanvasNavbar"
             aria-labelledby="offcanvasNavbarLabel"
-            placement="end"
+            placement="start"
           >
             <Offcanvas.Header closeButton>
-              <Offcanvas.Title id="offcanvasNavbarLabel">
-                Offcanvas
-              </Offcanvas.Title>
+              <Offcanvas.Title id="offcanvasNavbarLabel">Menu</Offcanvas.Title>
             </Offcanvas.Header>
             <Offcanvas.Body>
-              <Nav className="justify-content-end flex-grow-1 pe-3">
-                <Nav.Link href="#action1">Home</Nav.Link>
-                <Nav.Link href="#action2">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
+              <Nav className="justify-content-start flex-grow-1 pe-3">
+                <Link to="/" className="nav-link">
+                  Accueil
+                </Link>
+                <Link to="/test" className="nav-link">
+                  Clients
+                </Link>
+                <NavDropdown
+                  title="Dropdown"
+                  id="offcanvasNavbarDropdown"
+                  className="ms-auto"
+                >
                   <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
                   <NavDropdown.Item href="#action4">
                     Another action
@@ -38,15 +47,6 @@ export function AppNavbar() {
                   </NavDropdown.Item>
                 </NavDropdown>
               </Nav>
-              <Form className="d-flex">
-                <Form.Control
-                  type="search"
-                  placeholder="Search"
-                  className="me-2"
-                  aria-label="Search"
-                />
-                <Button variant="outline-success">Search</Button>
-              </Form>
             </Offcanvas.Body>
           </Navbar.Offcanvas>
         </Container>
