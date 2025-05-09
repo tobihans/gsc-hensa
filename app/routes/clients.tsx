ac7e8ce275dba71835965a072f41676a6fabcebd
jouvence13 <spaceweb1997.com>
2025-05-09 10:04:27 +0100

    core: dele tetest

--- app/routes/clients.tsx
+++ app/routes/clients.tsx
@@ -2,7 +2,8 @@ export default function clients() {
 
   return (
     <div>
-        <p>ma pages clients</p>
+        <h1>ma pages clients</h1>
+        <p>Voici la liste de mes clients</p>
     </div>
   );
 }
