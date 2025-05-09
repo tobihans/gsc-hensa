c431a79318f81db2bbb7e0f291b366676c4e100d
Lionel <lioneltoglobesse@gmail.com>
2025-05-09 12:05:34 +0100

    wip: load clients list from firebase

--- app/routes/clients.tsx
+++ app/routes/clients.tsx
@@ -1,8 +1,20 @@
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "~/firebase.config";
 
 export default function Clients() {
+    // TDDO:Utiliser un cliient Loader pour charger les clients
+    getDocs(collection(db, "clients"))
+        .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                console.log(`${doc.id} => ${doc.data()}`)
+                ;
+            })
+        })
+   /* */
+
     return (
         <Row className=" w-100 d-flex justify-content-between mb-3 ">
             <Col xs={6} md={11}>
