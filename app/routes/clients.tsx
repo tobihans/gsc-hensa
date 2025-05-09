84800a55cc815ae4c0f143cab40b2547d0920c98
Daiki67 <ndjambee@gmail.com>
2025-05-09 11:11:31 +0100

    wip: title of clients page

--- app/routes/clients.tsx
+++ app/routes/clients.tsx
@@ -1,7 +1,15 @@
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 export default function Clients() {
     return(
-        <div>
-            <h1>Clients</h1>
-        </div>
+        <Row className="row w-100 d-flex justify-content-between align-items-center">
+            <Col xs="6" md="11">
+                <h2>Liste des clients</h2>
+            </Col>
+            <Col xs={6} md="1">
+                <Button variant="primary" className="w-auto">Ajouter</Button>
+            </Col>
+        </Row>
     );
 }
