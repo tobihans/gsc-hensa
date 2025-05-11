32c03c70a6229144b47ca008d3e736a788232b65
Hans B. K. Tognon <git@tobihans.space>
2025-05-11 23:59:53 +0100

    feat(clients): add ability to list clients

---
+++ app/data/models/client.ts
@@ -0,0 +1,45 @@
+import type { FirestoreDataConverter } from "firebase/firestore";
+
+export interface Client {
+  uid?: string;
+  fullname: string;
+  email: string;
+  phone: string;
+  address:
+  | {
+    country: string;
+    city: string;
+    street: string;
+    postalCode: string;
+  }
+  | string;
+  createdAt: Date;
+}
+
+export namespace ClientRouter {
+  export interface LoaderData {
+    clients: Client[];
+  }
+}
+
+export const clientConverter: FirestoreDataConverter<Client> = {
+  toFirestore: (client) => ({
+    fullname: client.fullname,
+    email: client.email,
+    phone: client.phone,
+    address: client.address,
+    createdAt: client.createdAt,
+  }),
+  fromFirestore: (snapshot, options) => {
+    const data = snapshot.data(options);
+
+    return {
+      uid: snapshot.id,
+      fullname: data.fullname,
+      email: data.email,
+      phone: data.phone,
+      address: `${data.address.postalCode}, ${data.address.street}, ${data.address.city}, ${data.address.country}`,
+      createdAt: data.createdAt.toDate(),
+    };
+  },
+};
--- app/routes/clients.tsx
+++ app/routes/clients.tsx
@@ -2,23 +2,61 @@ import { collection, getDocs } from "firebase/firestore";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import Table from "react-bootstrap/Table";
+import { clientConverter } from "~/data/models/client";
 import { db } from "~/firebase.config";
+import type { Route } from "./+types/clients";
 
 export const clientLoader = async () => {
-  /* */
+  const clientsRef = collection(db, "clients").withConverter(clientConverter);
+  const querySnapshot = await getDocs(clientsRef);
+  const clients = querySnapshot.docs.map((doc) => doc.data());
+
+  return { clients };
 };
 
-export default function Clients() {
+export default function Clients({ loaderData }: Route.ComponentProps) {
+  const clients = loaderData.clients;
+  console.log(loaderData.clients);
+
   return (
-    <Row className=" w-100 d-flex justify-content-between mb-3 ">
-      <Col xs={6} md={11}>
-        <h2>Liste des clients</h2>
-      </Col>
-      <Col xs={6} md={1}>
-        <Button variant="primary" className="w-auto">
-          Ajouter
-        </Button>
-      </Col>
-    </Row>
+    <>
+      <Row className="w-100 d-flex justify-content-between mb-5">
+        <Col xs={6} md={11}>
+          <h2>Liste des clients</h2>
+        </Col>
+        <Col xs={6} md={1}>
+          <Button variant="primary" className="w-auto">
+            Ajouter
+          </Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Table>
+            <thead>
+              <tr>
+                <th>Nom</th>
+                <th>Email</th>
+                <th>Téléphone</th>
+                <th>Adresse</th>
+                <th>Ajouté le</th>
+              </tr>
+            </thead>
+            <tbody>
+              {clients.map((client) => (
+                <tr key={client.uid}>
+                  <td>{client.fullname}</td>
+                  <td>{client.email}</td>
+                  <td>{client.phone}</td>
+                  <td>{String(client.address)}</td>
+                  <td>{String(client.createdAt.toLocaleString())}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+    </>
   );
 }
