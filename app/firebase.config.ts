b2aca4018117b956cd3deca4161bd4b989f2cbea
Hans B. K. Tognon <git@tobihans.space>
2025-05-16 00:53:57 +0100

    feat: initialize google analytics

--- app/firebase.config.ts
+++ app/firebase.config.ts
@@ -1,3 +1,4 @@
+import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -20,3 +21,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const analytics = getAnalytics(app);
